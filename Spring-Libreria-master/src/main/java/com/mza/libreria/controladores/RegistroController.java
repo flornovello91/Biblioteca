27effdfa01330ac4406be0b6f9058b4aa79e288e
Florencia Novello <flornovello91@gmail.com>
2021-12-05 10:39:50 -0300

    developing

--- Spring-Libreria-master/src/main/java/com/mza/libreria/controladores/RegistroController.java
+++ Spring-Libreria-master/src/main/java/com/mza/libreria/controladores/RegistroController.java
@@ -7,11 +7,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class RegistroController {
     
     @Autowired
