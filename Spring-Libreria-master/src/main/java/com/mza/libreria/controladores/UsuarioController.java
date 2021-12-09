d42bce8abb5b1275c3de775c83280abbe367dd68
Florencia Novello <flornovello91@gmail.com>
2021-12-09 20:45:44 -0300

    registro vista

--- Spring-Libreria-master/src/main/java/com/mza/libreria/controladores/UsuarioController.java
+++ Spring-Libreria-master/src/main/java/com/mza/libreria/controladores/UsuarioController.java
@@ -18,7 +18,7 @@ public class UsuarioController {
     
     @GetMapping("/registro")
     public String registro(){
-        return "registro";
+        return "registro.html";
     }
     
     @PostMapping("/registro")
@@ -26,10 +26,10 @@ public class UsuarioController {
         try {
             usuarioService.registrarUsuario(nombre, apellido, email, contraseña);
             //modelo.put("exito", "Guardado de manera exitosa!");
-            return ("registro");
+            return ("registro.html");
         }catch (Exception e){
             //modelo.put("error", "Error en la carga del libro.");
-            return ("registro");
+            return ("registro.html");
         }
     }
     
@@ -39,29 +39,3 @@ public class UsuarioController {
     }
 
 }
-/*
-@PostMapping("/registro")
-    public String registrar(ModelMap modelo,
-                            @RequestParam String nombre,
-                            @RequestParam String apellido,
-                            @RequestParam String email,
-                            @RequestParam String clave1,
-                            @RequestParam String clave2) {
-
-        try {
-            sUsuario.registrar(nombre, apellido, email, clave1, clave2);
-        } catch (MiExcepcion ex) {
-
-            modelo.put("error", ex.getMessage());
-            modelo.put("nombre", nombre);
-            modelo.put("apellido", apellido);
-            modelo.put("email", email);
-            modelo.put("clave1", clave1);
-            modelo.put("clave2", clave2);
-            return "nUsuario";
-        }
-        modelo.put("titulo", "Bienvenido al Sistema de Gestion de Biblioteca");
-        modelo.put("descripcion", "Tu usuario fue registrado correctamente");
-        return "registro-exitoso";
-    }
-*/
